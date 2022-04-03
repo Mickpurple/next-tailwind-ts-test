6d4fb46c1d1001414057ccc7f25b076adf2a3b3c
Mickpurple <themickpurple@gmail.com>
2022-04-03 18:50:38 +0530

    1st commit

---
+++ pages/work.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+
+const Work: NextPage = () => {
+  return (
+<div className="flex items-center justify-center h-screen flex-wrap mx-auto mt-5">
+
+	
+	<img src="https://mfiles.alphacoders.com/820/820288.jpg" className="m-2 h-3/5 rounded-lg shadow-2xl" />
+  <img src="https://mfiles.alphacoders.com/820/820288.jpg" className="m-2 h-4/6 rounded-lg shadow-2xl" />
+  <img src="https://mfiles.alphacoders.com/820/820288.jpg" className="m-2 h-3/6 rounded-lg shadow-2xl" />
+  <img src="https://mfiles.alphacoders.com/820/820288.jpg" className="m-2 h-1/3 rounded-lg shadow-2xl" />
+
+		
+
+</div>
+  )
+}
+
+export default Work
